d9a7dae42330869de04b24462a04f7a16d97f5f3
Egor <nounstep@gmail.com>
2024-05-24 20:38:50 +0300

    Fix: VEC

--- handlers/VEC-handler.go
+++ handlers/VEC-handler.go
@@ -6,11 +6,16 @@ import (
 	types "psychward/src"
 )
 
+type VECField struct {
+	Value    int `json:"value"`
+	MaxValue int `json:"max_value"`
+}
+
 type VECResult struct {
-	AP          int    `json:"Абсолютный показатель (сумма результатов по субтестам)"`
-	KP          int    `json:"Корригированный на возраст показатель"`
-	IPP         int    `json:"Эквивалентный интеллекту показатель памяти"`
-	Description string `json:"description"`
+	AP          VECField `json:"Абсолютный показатель (сумма результатов по субтестам)"`
+	KP          VECField `json:"Корригированный на возраст показатель"`
+	IPP         VECField `json:"Эквивалентный интеллекту показатель памяти"`
+	Description string   `json:"description"`
 }
 
 func getKP(AP int) int {
@@ -50,144 +55,144 @@ func getKP(AP int) int {
 
 func getIPP(KP int) int {
 	IPP := 0
-	switch KP {
-	case 50:
+	switch {
+	case KP <= 50:
 		IPP = 48
-	case 51:
+	case KP == 51:
 		IPP = 49
-	case 52:
+	case KP == 52:
 		IPP = 49
-	case 53:
+	case KP == 53:
 		IPP = 50
-	case 54:
+	case KP == 54:
 		IPP = 51
-	case 55:
+	case KP == 55:
 		IPP = 52
-	case 56:
+	case KP == 56:
 		IPP = 52
-	case 57:
+	case KP == 57:
 		IPP = 53
-	case 58:
+	case KP == 58:
 		IPP = 54
-	case 59:
+	case KP == 59:
 		IPP = 55
-	case 60:
+	case KP == 60:
 		IPP = 55
-	case 61:
+	case KP == 61:
 		IPP = 56
-	case 62:
+	case KP == 62:
 		IPP = 57
-	case 63:
+	case KP == 63:
 		IPP = 57
-	case 64:
+	case KP == 64:
 		IPP = 58
-	case 65:
+	case KP == 65:
 		IPP = 59
-	case 66:
+	case KP == 66:
 		IPP = 59
-	case 67:
+	case KP == 67:
 		IPP = 60
-	case 68:
+	case KP == 68:
 		IPP = 61
-	case 69:
+	case KP == 69:
 		IPP = 62
-	case 70:
+	case KP == 70:
 		IPP = 62
-	case 71:
+	case KP == 71:
 		IPP = 63
-	case 72:
+	case KP == 72:
 		IPP = 64
-	case 73:
+	case KP == 73:
 		IPP = 64
-	case 74:
+	case KP == 74:
 		IPP = 66
-	case 75:
+	case KP == 75:
 		IPP = 67
-	case 76:
+	case KP == 76:
 		IPP = 69
-	case 77:
+	case KP == 77:
 		IPP = 70
-	case 78:
+	case KP == 78:
 		IPP = 72
-	case 79:
+	case KP == 79:
 		IPP = 73
-	case 80:
+	case KP == 80:
 		IPP = 74
-	case 81:
+	case KP == 81:
 		IPP = 76
-	case 82:
+	case KP == 82:
 		IPP = 77
-	case 83:
+	case KP == 83:
 		IPP = 79
-	case 84:
+	case KP == 84:
 		IPP = 80
-	case 85:
+	case KP == 85:
 		IPP = 81
-	case 86:
+	case KP == 86:
 		IPP = 84
-	case 87:
+	case KP == 87:
 		IPP = 84
-	case 88:
+	case KP == 88:
 		IPP = 86
-	case 89:
+	case KP == 89:
 		IPP = 87
-	case 90:
+	case KP == 90:
 		IPP = 89
-	case 91:
+	case KP == 91:
 		IPP = 90
-	case 92:
+	case KP == 92:
 		IPP = 92
-	case 93:
+	case KP == 93:
 		IPP = 93
-	case 94:
+	case KP == 94:
 		IPP = 94
-	case 95:
+	case KP == 95:
 		IPP = 96
-	case 96:
+	case KP == 96:
 		IPP = 97
-	case 97:
+	case KP == 97:
 		IPP = 99
-	case 98:
+	case KP == 98:
 		IPP = 100
-	case 99:
+	case KP == 99:
 		IPP = 101
-	case 100:
+	case KP == 100:
 		IPP = 103
-	case 101:
+	case KP == 101:
 		IPP = 105
-	case 102:
+	case KP == 102:
 		IPP = 106
-	case 103:
+	case KP == 103:
 		IPP = 108
-	case 104:
+	case KP == 104:
 		IPP = 110
-	case 105:
+	case KP == 105:
 		IPP = 112
-	case 106:
+	case KP == 106:
 		IPP = 114
-	case 107:
+	case KP == 107:
 		IPP = 116
-	case 108:
+	case KP == 108:
 		IPP = 118
-	case 109:
+	case KP == 109:
 		IPP = 120
-	case 110:
+	case KP == 110:
 		IPP = 122
-	case 111:
+	case KP == 111:
 		IPP = 124
-	case 112:
+	case KP == 112:
 		IPP = 128
-	case 113:
+	case KP == 113:
 		IPP = 129
-	case 114:
+	case KP == 114:
 		IPP = 132
-	case 115:
+	case KP == 115:
 		IPP = 135
-	case 116:
+	case KP == 116:
 		IPP = 137
-	case 117:
+	case KP == 117:
 		IPP = 140
-	case 118:
+	case KP >= 118:
 		IPP = 143
 	}
 	
@@ -249,7 +254,16 @@ func VECHandler(s *types.SurveyResults) []byte {
 		desc += "/ полная уо (идиотия)"
 	}
 	
-	result := VECResult{AP, KP, IPP, desc}
+	result := VECResult{VECField{
+		Value:    AP,
+		MaxValue: 93,
+	}, VECField{
+		Value:    KP,
+		MaxValue: 93 + 56,
+	}, VECField{
+		Value:    IPP,
+		MaxValue: 143,
+	}, desc}
 	resultJSON, err := json.Marshal(result)
 	if err != nil {
 		log.Fatalln(err)
